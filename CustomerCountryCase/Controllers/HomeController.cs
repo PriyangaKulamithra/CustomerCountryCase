c1c536549623f6850d43e7dd03139ba8c0d71c6d
Priyanga Kulamithra <priyanga.kulamithra@gmail.com>
2022-02-20 12:17:13 +0100

    Displaying customers on homepage

--- CustomerCountryCase/Controllers/HomeController.cs
+++ CustomerCountryCase/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using CustomerCountryCase.Data;
 using CustomerCountryCase.Models;
+using CustomerCountryCase.Services;
+using CustomerCountryCase.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace CustomerCountryCase.Controllers
@@ -13,23 +14,45 @@ namespace CustomerCountryCase.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IConfiguration _config;
+        private readonly ICustomerRepository _customerRepository;
+        private readonly ICountryRepository _countryRepository;
 
-        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        public HomeController(
+            ILogger<HomeController> logger,
+            ICustomerRepository customerRepository, 
+            ICountryRepository countryRepository)
         {
             _logger = logger;
-            _config = config;
+            _customerRepository = customerRepository;
+            _countryRepository = countryRepository;
         }
 
         public IActionResult Index()
         {
-            //var shortie = _config.GetConnectionString("Database");
-            return View();
+            var viewmodel = new HomeIndexViewModel
+            {
+                RegisteredCustomers = GetAllCustomers(),
+                Countries = GetCountrySelectListItems()
+            };
+            return View(viewmodel);
         }
 
-        public IActionResult Privacy()
+        private IEnumerable<CustomerDto> GetAllCustomers()
         {
-            return View();
+            return _customerRepository.GetAllRegistered().Select(c => new CustomerDto
+            {
+                CompanyName = c.CompanyName, CountryId = c.CountryId.ToString()
+            });
+        }
+
+        private IEnumerable<SelectListItem> GetCountrySelectListItems()
+        {
+            var list = _countryRepository.GetAllCountries()
+                .Select(country => new SelectListItem {Text = country.Country1, Value = country.Id.ToString()}).ToList();
+
+            list.Add(new SelectListItem{ Text = "Choose country", Value = "0", Disabled = true});
+
+            return list;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ CustomerCountryCase/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CustomerCountryCase.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CustomerCountryCase.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<CustomerDto> RegisteredCustomers{ get; set; }
+        public IEnumerable<SelectListItem> Countries { get; set; }
+    }
+}
