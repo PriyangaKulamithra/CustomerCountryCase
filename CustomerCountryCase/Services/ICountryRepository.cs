064707ac22a31c77be4ed70b4b5eaf71509ef91a
Priyanga Kulamithra <priyanga.kulamithra@gmail.com>
2022-02-20 10:38:01 +0100

    Repository interface added

---
+++ CustomerCountryCase/Services/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerCountryCase.Data;
+
+namespace CustomerCountryCase.Services
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> GetAllCountries();
+    }
+}
---
+++ CustomerCountryCase/Services/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerCountryCase.Data;
+
+namespace CustomerCountryCase.Services
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAllRegistered();
+        Customer Get(string id);
+        void Add(string name, string countryId);
+        void Remove(string id);
+    }
+}
